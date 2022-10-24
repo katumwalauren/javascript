23cbfda9f7f5d0cf9b7259a1259a40719d24b21f
epicallan <epicallan.al@gmail.com>
2022-10-25 02:43:16 +0400

    Add exercise-1

---
+++ exercise-1.mjs
@@ -0,0 +1,7 @@
+// 1. write a function named readStudents that returns the data in read data.json as an array
+
+
+// 2. Write a function named sortByFirstName that sorts students data by firstName
+
+
+// 2. Write a function that returns the oldest student
