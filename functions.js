8b037c16f61ee80bf9172e5bfe858b47744de4ae
katumwalauren <95852478+katumwalauren@users.noreply.github.com>
2022-11-04 19:37:23 +0300

    mjs Work

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
-
-
+//const fetch = require('node-fetch');
+//const fs = require('fs')
 
 //day and time list
 var daylist =["Sunday", "Monday", "Tuesday", "wednesday", "Thursday", "Friday", "saturday"]
@@ -157,6 +157,88 @@ function printMyName(MyName) {
   }
   console.log(printMyName('Lauren'))
 
-  //
+  //function returning row and column eg matrix
+  function matrix (n){
+    var  i;
+    var j;
+    for (var i = 0; i < n; i++) {
+        for( var j = 0; j< n; j++){
+            if ( i === j) {
+                console.log('1');
+            }
+            else 
+                console.log('0');
+        }
+    console.log ('-----');
+  
+    }
+  }
+matrix(3)
+
+//array of number to find 2 lowest amd 2greatest
+
+
+//promise with async and await
+// 1. promise is a special kind of javascript object that contains another object
+// it is formed of three states : fulfilled, reject and resolve
+const promise = new Promise((resolve, reject) =>{
+    const string1 = " coding is good"
+    const string2 = " coding is good"
+    if (string1 === string2 ) {
+    resolve()
+    } else {
+        reject()
+    }
+})
+promise
+    .then(() => console.log('success'))
+    .catch(() => console.error('something is wrong'));
+
+    //fs readfile with promise
+
+    fs.promises.readFile('./test.txt', {encoding : 'utf-8' })
+          .then((data) => console.log(data))
+          .catch((err) => console.error(err))
+
+//asynchronous : is placed before the function eg 
+async function testIt(){
+    return 'Lauren'
+} 
+testIt()
+
+//await is used to wait for a promise and it can only be used inside an async function eg
+// async function testThat (){
+//     let promise = thisPromise((resolve, reject) => {
+//         lastName(() => resolve('done'), 'Lauren')
+//     })
+//     let result = await promise
+//     console.log(result)
+// }
+// testThat()
+
+//callback function 1
+function myName (message, callback){
+    console.log("first run: " + message);
+    if (typeof callback == "function")
+        callback();
+}
+
+myName("Hello Lauren", function callfunction(){
+    console.log("this is a callback function. ")
+});
+
+// 2
+function firstOne() {
+    console.log("first name");
+}
+
+function secondOne (){
+    console.log("last name");
+}
+
+setTimeout(firstOne, 1000);
+secondOne();
+
+
 
   
