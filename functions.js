5209e3cff987a155c707502191b51c304901a099
katumwalauren <95852478+katumwalauren@users.noreply.github.com>
2022-11-11 13:20:09 +0300

    new json

--- functions.js
+++ functions.js
@@ -2,43 +2,40 @@
 //const fs = require('fs')
 
 //day and time list
-var daylist =["Sunday", "Monday", "Tuesday", "wednesday", "Thursday", "Friday", "saturday"]
-var today  = new Date();
+var daylist = ["Sunday", "Monday", "Tuesday", "wednesday", "Thursday", "Friday", "saturday"]
+var today = new Date();
 var day = today.getDay();
 console.log("Today is:" + daylist[day] + ".")
 var hour = today.getHours();
 var minute = today.getMinutes();
 var second = today.getSeconds();
-var prepand = (hour >= 12)? " PM ":" AM ";
-hour = (hour >= 12)? hour - 12: hour;
-if (hour === 0 && prepand ===' PM ') 
-{ 
-if (minute === 0 && second === 0)
-{ 
-hour=12;
-prepand=' Noon';
-} 
-else
-{ 
-hour=12;
-prepand=' PM';
-} 
+var prepand = (hour >= 12) ? " PM " : " AM ";
+hour = (hour >= 12) ? hour - 12 : hour;
+if (hour === 0 && prepand === ' PM ') {
+    if (minute === 0 && second === 0) {
+        hour = 12;
+        prepand = ' Noon';
+    }
+    else {
+        hour = 12;
+        prepand = ' PM';
+    }
 }
-console.log("Current Time : "+hour + prepand + " : " + minute + " : " + second)
+console.log("Current Time : " + hour + prepand + " : " + minute + " : " + second)
 
 //reverse a function
 function reverse_a_number(i) {
     i = i + "";
     return i.split("").reverse().join("");
-  }
-  console.log(Number(reverse_a_number(32243)));
+}
+console.log(Number(reverse_a_number(32243)));
 
-  // program to check if the string is palindrome or not
+// program to check if the string is palindrome or not
 function checkPalindrome(string) {
     const arrayValues = string.split('');
     const reverseArrayValues = arrayValues.reverse();
     const reverseString = reverseArrayValues.join('');
-    if(string == reverseString) {
+    if (string == reverseString) {
         console.log('It is a palindrome');
     }
     else {
@@ -48,19 +45,19 @@ function checkPalindrome(string) {
 const string = checkPalindrome('Lauren')
 
 //function that generates all combination of a string
-function combinator (s) {
-   var  list_of_strings = new Array();
-    for( var i=0;i<s.length;i++) {
-        for(var j=i+1;j<s.length+1;j++) {
+function combinator(s) {
+    var list_of_strings = new Array();
+    for (var i = 0; i < s.length; i++) {
+        for (var j = i + 1; j < s.length + 1; j++) {
             list_of_strings.push(s.slice(i, j));
         }
     }
     return list_of_strings;
- }
+}
 console.log(combinator("dog"))
 
 //arrange words in alphabet
-function sortThis(str){
+function sortThis(str) {
     var arr = str.split('')
     var sorted = arr.sort()
     return sorted.join('')
@@ -73,9 +70,9 @@ const str2 = str.charAt(0).toUpperCase() + str.slice(1);
 console.log(str2)
 
 //finding the longest word on a string
-function longest (str) {
+function longest(str) {
     str = str.split(" ")
-    return str.sort((a,b)  => b.length - a.length)[0]
+    return str.sort((a, b) => b.length - a.length)[0]
 }
 console.log(longest("love the goodmrng word"))
 
@@ -84,7 +81,7 @@ function countVowel(str) {
     var vowelList = 'aeiouAEIOU';
     var vCount = 0
     for (var x = 0; x < str.length; x++) {
-        if(vowelList.indexOf(str[x]) !== -1){
+        if (vowelList.indexOf(str[x]) !== -1) {
             vCount += 1
         }
     }
@@ -93,39 +90,38 @@ function countVowel(str) {
 console.log(countVowel("my name is Lauren"))
 
 //check prime
-function getPrime (n) {
-    for(var x = 2; x < n; x++)
-    {
-        if (n % x === 0){
+function getPrime(n) {
+    for (var x = 2; x < n; x++) {
+        if (n % x === 0) {
             return 'is not prime'
         }
     }
-        return 'is prime'
+    return 'is prime'
 }
-console.log (getPrime(4))
+console.log(getPrime(4))
 
 //returning types in javascript
-function javaType (value){
-    var dataType =[Function, Number, String, Boolean, Object], x, length;
-    if(typeof value === "object" || typeof value === "function"){
-        for ( x = 0, length = dataType.length; x < length; x++){
-            if (value instanceof dataType[x]){
+function javaType(value) {
+    var dataType = [Function, Number, String, Boolean, Object], x, length;
+    if (typeof value === "object" || typeof value === "function") {
+        for (x = 0, length = dataType.length; x < length; x++) {
+            if (value instanceof dataType[x]) {
                 return dataType[x]
             }
         }
     }
     return typeof value
 }
-console.log (javaType(5));
+console.log(javaType(5));
 console.log(javaType('Lauren'));
 console.log(javaType('true'))
 
 //function taking in  numbers variable an double it if even
-function doubleEven (number){
+function doubleEven(number) {
     const newNumber = [];
-    for(var i = 0; i < number.length; i++){
-        if( number[i] % 2 === 0) {
-            newNumber.push (number[i] * 2);
+    for (var i = 0; i < number.length; i++) {
+        if (number[i] % 2 === 0) {
+            newNumber.push(number[i] * 2);
         }
     }
     return newNumber
@@ -133,11 +129,11 @@ function doubleEven (number){
 console.log(doubleEven([1, 2, 3, 4, 5, 6]))
 
 //function taking in  numbers variable and squares it if odd
-function squareOdd (number){
+function squareOdd(number) {
     const thatNumber = [];
-    for(var i = 0; i < number.length; i++){
-        if( number[i] % 2 != 0) {
-            thatNumber.push (number[i] * number[i]);
+    for (var i = 0; i < number.length; i++) {
+        if (number[i] % 2 != 0) {
+            thatNumber.push(number[i] * number[i]);
         }
     }
     return thatNumber
@@ -145,7 +141,7 @@ function squareOdd (number){
 console.log(squareOdd([1, 2, 3, 4, 5, 6]))
 
 //function taking in a num variable and doubles it
-function doubleIt(num){
+function doubleIt(num) {
     return num * num;
 }
 console.log(doubleIt(7))
@@ -154,29 +150,28 @@ console.log(doubleIt(7))
 function printMyName(MyName) {
     const name = 'Lauren'
     return "Hello" + " " + name;
-  }
-  console.log(printMyName('Lauren'))
+}
+console.log(printMyName('Lauren'))
 
-  //function returning row and column eg matrix
-  function matrix (n){
-    var  i;
+//function returning row and column eg matrix
+function matrix(n) {
+    var i;
     var j;
     for (var i = 0; i < n; i++) {
-        for( var j = 0; j< n; j++){
-            if ( i === j) {
+        for (var j = 0; j < n; j++) {
+            if (i === j) {
                 console.log('1');
             }
-            else 
+            else
                 console.log('0');
         }
-    console.log ('-----');
-  
+        console.log('-----');
+
     }
-  }
+}
 matrix(3)
 
 //array of number to find 2 lowest amd 2greatest
 
 
 
-  
--- read-json.mjs
+++ read-json.mjs
@@ -2,13 +2,13 @@ import fs from 'fs'
 import sortBy from 'lodash/sortBy.js'
 
 //callback function 1
-function myName (message, callback){
+function myName(message, callback) {
     console.log("first run: " + message);
     if (typeof callback == "function")
         callback();
 }
 
-myName("Hello Lauren", function callfunction(){
+myName("Hello Lauren", function callfunction() {
     console.log("this is a callback function. ")
 });
 
@@ -17,7 +17,7 @@ function firstOne() {
     console.log("first name");
 }
 
-function secondOne (){
+function secondOne() {
     console.log("last name");
 }
 
@@ -27,30 +27,90 @@ secondOne();
 
 //exercises
 // 1. write a function named readStudents that returns the data in read data.json as an array
-fs.readFile('data.json', 'utf8', (error, data) => {
-    if(error){
-       console.log(error);
-       return;
-    }
-    console.log(JSON.parse(data));
+function readStudents() {
+    fs.readFile('data.json', 'utf8', (error, data) => {
+        if (error) {
 
-})
+            console.log(error);
+
+            return;
+        }
+        console.log(JSON.parse(data));
+
+    })
+}
+readStudents();
+
 // 2. Write a function named sortByFirstName that sorts students data by firstName
-fs.readFile('data.json', 'utf8', (error, sortByFirstName) => {
- var data =  JSON.parse(sortByFirstName) 
- function data(name) {
-    var arr = [];
-    arr.name = name;
-    return arr;
-  }
+//1 method
+function sortByFirstName() {
+    fs.readFile('data.json', 'utf8', (error, sortByFirstName) => {
 
-})
+        var data = JSON.parse(sortByFirstName)
+
+        var name = data.sort(
+
+            function (first, last) {
+
+                if (first.name.toLowerCase() > last.name.toLowerCase()
+
+                ) return 1;
+
+                if (first.name.toLowerCase() < last.name.toLowerCase()
+
+                ) return -1;
+
+                return 0;
+            })
+
+        console.log(name)
+    })
+}
+sortByFirstName();
+
+//by name without using functions
+function getName() {
+
+    fs.readFile('data.json', 'utf8', (error, names) => {
+
+        var data = JSON.parse(names)
+
+        let sortByName = data.map((item) => [item["name"], item])
+
+        console.log("sortByName", sortByName)
+    })
+}
+getName();
+
+//2 method
+function sortByName() {
+    fs.readFile('data.json', (err, rawData) => {
+        if (err) {
+            console.log(err);
+        }
+
+        const data = JSON.parse(rawData);
+
+        const sortedData = data.sort((first, last) => {
+            return first.name.toLowerCase() - last.name.toLowerCase();
+        });
+
+        console.log(sortedData);
+    });
+}
+sortByName();
 
 // 3. Write a function that returns the oldest student
-fs.readFile('data.json', 'utf8', (error, rawData) => {
-var data =  JSON.parse(rawData)   
-var age = data.sort(function(a, b) { 
- return b.age - a.age;   
-})  
-console.log(age[0])
-})
+function oldest() {
+    fs.readFile('data.json', 'utf8', (error, rawData) => {
+
+        var data = JSON.parse(rawData)
+
+        var age = data.sort(function (a, b) {
+
+            return b.age - a.age;
+        })
+        console.log(age[0])
+    })
+}
+oldest();
