29afd1818fb90f80970d0dc77c8b26cdfe81b601
katumwalauren <95852478+katumwalauren@users.noreply.github.com>
2022-12-04 00:12:05 +0300

    new work

--- read2-json.mjs
+++ read2-json.mjs
@@ -131,7 +131,7 @@ function addProperties2() {
                 return {...obj, educ:'secondary'};   
             }
         
-          });
+          })
           
           console.log(newArr);
 
